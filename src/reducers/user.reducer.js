3eca8bbf2273b78fd38373a9e01e19e5702c5872
Laurent Bigey <laurent.bigey@outlook.fr>
2024-02-27 20:10:54 +0100

    test master

--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -3,6 +3,8 @@ import { GET_USER_INFO, UPDATE_USER_INFO } from "../actions/user.action"
 
 const initialState = {}
 
+//cette branche est la plus récente
+
 export default function userReducer(state = initialState, action) {
     switch (action.type) {
         case GET_USER_INFO:
